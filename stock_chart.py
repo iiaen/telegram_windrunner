16ffe6f0a228082724bfafc55fe4886d5816f87d
Jia <tyforfeedback@gmail.com>
2021-03-13 18:50:47 +0800

    First commit

---
+++ stock_chart.py
@@ -0,0 +1,14 @@
+import pyTelegramBotAPI 
+from telethon.sync import TelegramClient 
+# from telethon.tl.types import InputPeerUser, InputPeerChannel 
+from telethon import TelegramClient, sync, events 
+from credentials import api_id, api_hash, bot_token, phone
+
+
+# creating a telegram session and assigning it to a variable client 
+client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+   
+# connecting and building the session 
+client.connect() 
+client.send_message('iiaen', 'Send message to @<bot> without signing in with OTP') 
+client.send_file('iiaen', 'C:\\Users\\iiaen\\SWRD.png') 
